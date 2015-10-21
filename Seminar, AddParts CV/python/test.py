b63f4895cec4896679c47247992c9b4bb5659d36
Andrew Belyaev <abelyaev.vmk@gmail.com>
2015-10-21 21:20:29 +0300

    21-10 End of Work

--- Seminar, AddParts CV/python/test.py
+++ Seminar, AddParts CV/python/test.py
@@ -1,21 +1,20 @@
+import numpy as np
+import scipy
+from scipy.misc import imread
 import maxflow
-# Create a graph with integer capacities.
-g = maxflow.Graph[int](2, 2)
-# Add two (non-terminal) nodes. Get the index to the first one.
-nodes = g.add_nodes(2)
-# Create two edges (forwards and backwards) with the given capacities.
-# The indices of the nodes are always consecutive.
-g.add_edge(nodes[0], nodes[1], 1, 2)
-# Set the capacities of the terminal edges...
-# ...for the first node.
-g.add_tedge(nodes[0], 2, 5)
-# ...for the second node.
-g.add_tedge(nodes[1], 9, 4)
 
-# Find the maxflow.
-flow = g.maxflow()
-print "Maximum flow:", flow
+img = imread("E:\\a2.png")
 
-# Print the segment of each node.
-print "Segment of the node 0:", g.get_segment(nodes[0])
-print "Segment of the node 1:", g.get_segment(nodes[1])
+g = maxflow.Graph[float]()
+nodeids = g.add_grid_nodes(img.shape)
+g.add_grid_edges(nodeids, 50)
+g.add_grid_tedges(nodeids, img, 255-img)
+g.maxflow()
+sgm = g.get_grid_segments(nodeids)
+img2 = np.int_(np.logical_not(sgm))
+
+from matplotlib import pyplot as ppl
+
+ppl.imshow(img2)
+ppl.show()
+
